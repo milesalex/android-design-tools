9ab6cff9fb61c5850fdbd29d6e3582c9a66df010
Alex Miles <alexmiles@dropbox.com>
2014-12-03 11:42:42 -0800

    initial gh-pages commit

---
+++ source/javascripts/scripts.js
@@ -0,0 +1,29 @@
+function getHexValue(){
+	var opacityInput = document.getElementById("opacity");
+
+	if (opacityInput.value == "") return false;
+
+	opacityInput = parseInt(opacityInput.value);
+
+	if ((opacityInput >= 0) && (opacityInput <= 100)) {
+		// Opacity input
+		opacityInput = opacityInput / 100;
+
+		// Opacity * color space
+		var alpha = opacityInput * 255;
+
+		// Round rgbA to nearest whole number
+		var roundedAlpha = Math.round(alpha);
+
+		// Convert transparency to hex
+		var hexString = roundedAlpha.toString(16);
+
+		// Hex output
+		var hexOutput = document.getElementById("hex");
+
+		hexOutput.innerText = "#" + hexString;
+	} else {
+		alert('Enter a number inbetween 0 and 100');
+	}
+
+}
